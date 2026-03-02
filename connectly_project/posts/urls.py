99ddc4c5971e6104c31f9feaac2f20437a79eed7
Michelle Joi Quesada <lr.mjquesada@mmdc.mcl.edu.ph>
2026-03-02 19:08:43 +0800

    Update views and urls to fix 404 and implement RBAC 403 Forbidden test

--- connectly_project/posts/urls.py
+++ connectly_project/posts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import UserListCreate, PostListCreate, CommentListCreate
+from .views import UserListCreate, PostListCreate, PostDetailView, CommentListCreate
 
 urlpatterns = [
     path('users/', UserListCreate.as_view(), name='user-list-create'),
     path('posts/', PostListCreate.as_view(), name='post-list-create'),
+    path('posts/<int:pk>/', PostDetailView.as_view(), name='post-detail'), 
     path('comments/', CommentListCreate.as_view(), name='comment-list-create'),
 ]
