d9961e661251de193670a0ba7af75def589938af
Sarah Nicole Hular <sarah@Sarahs-MacBook-Air.local>
2026-03-05 15:21:48 +0800

    Enhanced News Feed

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,17 +1,18 @@
 from rest_framework import serializers
-from django.contrib.auth.models import User 
+from django.contrib.auth.models import User
 from .models import Post, Comment, Like
 
+
+# ---------------- USER ----------------
+
 class UserSerializer(serializers.ModelSerializer):
-    # Idagdag ang password field at gawin itong write_only para hindi makita sa GET requests
     password = serializers.CharField(write_only=True)
 
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'password'] 
+        fields = ['id', 'username', 'email', 'password']
 
     def create(self, validated_data):
-        # GAMITIN ITO: create_user() ang nagha-hash ng password gamit ang Argon2/PBKDF2
         user = User.objects.create_user(
             username=validated_data['username'],
             email=validated_data.get('email', ''),
@@ -19,12 +20,8 @@ class UserSerializer(serializers.ModelSerializer):
         )
         return user
 
-class PostSerializer(serializers.ModelSerializer):
-    comments = serializers.StringRelatedField(many=True, read_only=True)
 
-    class Meta:
-        model = Post
-        fields = ['id', 'title', 'content', 'post_type', 'metadata', 'author', 'created_at', 'comments']
+# ---------------- COMMENT ----------------
 
 class CommentSerializer(serializers.ModelSerializer):
     class Meta:
@@ -32,7 +29,6 @@ class CommentSerializer(serializers.ModelSerializer):
         fields = ['id', 'text', 'author', 'post', 'created_at']
 
     def validate_post(self, value):
-        # Note: value dito ay object na, kaya value.id ang ginagamit
         if not Post.objects.filter(id=value.id).exists():
             raise serializers.ValidationError("Post not found.")
         return value
@@ -41,9 +37,44 @@ class CommentSerializer(serializers.ModelSerializer):
         if not User.objects.filter(id=value.id).exists():
             raise serializers.ValidationError("Author not found.")
         return value
-    
-# --- NEW: LikeSerializer ---
-    class LikeSerializer(serializers.ModelSerializer):
-     class Meta:
+
+
+# ---------------- LIKE ----------------
+
+class LikeSerializer(serializers.ModelSerializer):
+    class Meta:
         model = Like
         fields = ['id', 'user', 'post', 'created_at']
+
+
+# ---------------- POST ----------------
+
+class PostSerializer(serializers.ModelSerializer):
+    comments = CommentSerializer(many=True, read_only=True)
+    likes = LikeSerializer(many=True, read_only=True)
+
+    like_count = serializers.SerializerMethodField()
+    comment_count = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Post
+        fields = [
+            'id',
+            'title',
+            'content',
+            'post_type',
+            'metadata',
+            'author',
+            'created_at',
+            'likes',
+            'comments',
+            'like_count',
+            'comment_count'
+        ]
+
+
+    def get_like_count(self, obj):
+        return obj.likes.count()
+
+    def get_comment_count(self, obj):
+        return obj.comments.count()
